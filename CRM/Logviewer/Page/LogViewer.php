417d603a306974974a6bb3a1744ee61f82386d7e
Luciano Spiegel <l.spiegel@ixiam.com>
2017-10-27 16:19:15 +0200

    fix date regexp

--- CRM/Logviewer/Page/LogViewer.php
+++ CRM/Logviewer/Page/LogViewer.php
@@ -17,7 +17,7 @@ class CRM_Logviewer_Page_LogViewer extends CRM_Core_Page {
         $dd = fgets($handle);
         if (strlen($dd) >= 15 && (' ' != $dd[0])) {
           $date = substr($dd,0,15);
-          if (preg_match("/^[A-Za-z][a-z]{2} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$/",$date)) {
+          if (preg_match("/^[A-Za-z]{3} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$/",$date)) {
             $entry_url = CRM_Utils_System::url('civicrm/admin/logviewer/logentry', $query = 'lineNumber='.$line);
             $entries[$line] = array('lineNumber' => '<a href="'.$entry_url.'">'.$line.'</a>', 'dateTime' => $date, 'message' => substr($dd,16));
           }
