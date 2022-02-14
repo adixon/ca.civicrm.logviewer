5a15e18a98e21c6e73b88f2af3114654dc613554
Mathieu Lutfy <mathieu@symbiotic.coop>
2022-02-14 15:48:36 -0500

    Fix for localized dates (month names with unicode)

--- CRM/Logviewer/Page/LogViewer.php
+++ CRM/Logviewer/Page/LogViewer.php
@@ -16,8 +16,9 @@ class CRM_Logviewer_Page_LogViewer extends CRM_Core_Page {
         $line++;
         $dd = fgets($handle);
         if (strlen($dd) >= 15 && (' ' != $dd[0])) {
-          $date = substr($dd,0,15);
-          if (preg_match("/^[A-Za-z]{3} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$/",$date)) {
+          // Also support localized dates such as: "fév 14 12:51:13"
+          $date = mb_substr($dd,0,15);
+          if (preg_match("/^\w{3} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$/u",$date)) {
             $entry_url = CRM_Utils_System::url('civicrm/admin/logviewer/logentry', $query = 'lineNumber='.$line);
             $entries[$line] = array('lineNumber' => '<a href="'.$entry_url.'">'.$line.'</a>', 'dateTime' => $date, 'message' => substr($dd,16));
           }
