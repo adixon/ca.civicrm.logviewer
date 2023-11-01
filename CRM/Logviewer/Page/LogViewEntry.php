c1b890b37face69e26ecaf0c0942df06bd0fc751
Mathieu Lu <mathieu@symbiotic.coop>
2023-11-01 16:09:45 -0400

    fixes#16: support the CiviCRM 5.67 log format

--- CRM/Logviewer/Page/LogViewEntry.php
+++ CRM/Logviewer/Page/LogViewEntry.php
@@ -8,9 +8,7 @@ class CRM_Logviewer_Page_LogViewEntry extends CRM_Core_Page {
     // CRM_Core_Resources::singleton()->addScriptUrl('https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js');
     CRM_Core_Resources::singleton()->addScriptUrl('//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.11.0/highlight.min.js', 10, 'page-header');
     CRM_Core_Resources::singleton()->addStyleUrl('//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.11.0/styles/default.min.css');
-    $config = CRM_Core_Config::singleton();
     $file_log = CRM_Core_Error::createDebugLogger();
-    // print_r($file_log); die();
     $logFileName = $file_log->_filename;
     $logFileFormat = $file_log->_lineFormat;
     $file_log->close();
@@ -18,26 +16,26 @@ class CRM_Logviewer_Page_LogViewEntry extends CRM_Core_Page {
     $handle = fopen($logFileName,'r') or die ('File opening failed');
     $entry = '';
     $line = $prevLine = $nextLine = 0;
-    $lineNumber = CRM_Utils_Array::value('lineNumber', $_GET);
+    $lineNumber = $_GET['lineNumber'] ?? NULL;
     if (empty($lineNumber)) { die('Invalid lineNumber'); }
     while (!feof($handle)) {
       $line++;
       $dd = fgets($handle);
       if ($line < $lineNumber) {
-        if (preg_match("/^[A-Z][a-z]{2} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$/",substr($dd,0,15))) {
+        if (preg_match("/^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}:[0-9]{2}.\d+/", $dd)) {
           $prevLine = $line;
         }
         continue;
       }
       elseif ($line == $lineNumber) {
-        $date = substr($dd,0,15);
+        $date = substr($dd, 0, 24);
         $this->assign('dateTime', $date);
-        $entry = substr($dd, 16);
+        $entry = substr($dd, 26);
       }
       else {
-        if (strlen($dd) >= 15 && (' ' != $dd[0])) {
-          $date = substr($dd,0,15);
-          if (preg_match("/^[A-Z][a-z]{2} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$/",$date)) {
+        if (strlen($dd) >= 25 && (' ' != $dd[0])) {
+          $date = substr($dd, 0, 24);
+          if (preg_match("/^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}:[0-9]{2}.\d+$/",$date)) {
             $nextLine = $line;
             break;
           }
