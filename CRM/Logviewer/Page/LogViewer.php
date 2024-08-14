79d1f65fb9bf5e0b95988f6c0df2526c44f15c11
Gerhard Weber <gerhard.weber@civiservice.de>
2024-08-14 15:41:13 +0200

    New approach after sync with changes #30: Remove Quickform and include form in page.
    Also:
    * remove JS from template, instead add js/logviewer.js
    * add Reset button

--- CRM/Logviewer/Page/LogViewer.php
+++ CRM/Logviewer/Page/LogViewer.php
@@ -1,16 +1,74 @@
 <?php
 
+use CRM_Logviewer_ExtensionUtil as E;
+
+/**
+ *
+ */
 class CRM_Logviewer_Page_LogViewer extends CRM_Core_Page {
+
+  /**
+   * Log level file name.
+   *
+   * @var string
+   */
+  protected string $logFileName;
+
+  /**
+   * Log levels with Psr::Log::LogLevel as key and corresponding label as value,
+   * starting with emergency.
+   *
+   * @var array
+   */
+  protected array $logLevels;
+
+  public function __construct($title = NULL, $mode = NULL) {
+    $file_log = CRM_Core_Error::createDebugLogger();
+    $this->logFileName = is_callable(['CRM_Core_Error', 'generateLogFileName']) ? \CRM_Core_Error::generateLogFileName('') : $file_log->_filename;
+    $file_log->close();
+
+    $this->logLevels = $this->getLogLevels();
+
+    parent::__construct($title, $mode);
+  }
 
   public function run() {
     $this->assign('currentTime', date('Y-m-d H:i:s'));
-    $file_log = CRM_Core_Error::createDebugLogger();
-    $logFileName = is_callable(['CRM_Core_Error', 'generateLogFileName']) ? \CRM_Core_Error::generateLogFileName('') : $file_log->_filename;
-    $file_log->close();
-    $this->assign('fileName', $logFileName);
-    $entries = [];
+    $this->assign('fileName', $this->logFileName);
 
-    if ($handle = @fopen($logFileName,'r')) {
+    if (array_key_exists('logviewer_reset', $_POST)) {
+      // Simulate reset behaviour.
+      $filter_severity = [];
+    }
+    else {
+      $filter_severity = $_POST['severity'] ?? [];
+    }
+
+    // Build options.
+    $options = [];
+    foreach ($this->logLevels as $key => $label) {
+      $options[] = [
+        'key' => $key,
+        'label' => $label,
+        'selected' => in_array($key, $filter_severity),
+      ];
+    }
+    $this->assign('options', $options);
+
+    $severities_found = [];
+    foreach (array_keys($this->logLevels) as $log_level) {
+      $severities_found[$log_level] = 0;
+    }
+
+    // Get Log entries.
+    $entries = [];
+    $handle = @fopen($this->logFileName, 'r');
+    if (!$handle) {
+      $this->assign('logFound', FALSE);
+    }
+    else {
+      $this->assign('noSearch', FALSE);
+      $this->assign('logFound', TRUE);
       $line = 0;
       while (!feof($handle)) {
         $line++;
@@ -19,20 +77,66 @@ class CRM_Logviewer_Page_LogViewer extends CRM_Core_Page {
           // Ex: 2023-12-25 09:00:00-0400
           if (preg_match('/^([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}.\d+)/', $dd, $matches)) {
             $date = $matches[1];
-            $entry_url = CRM_Utils_System::url('civicrm/admin/logviewer/logentry', $query = 'lineNumber='.$line);
-            $entries[$line] = [
-              'lineNumber' => '<a href="'.$entry_url.'">'.$line.'</a>',
-              'dateTime' => $date,
-              'message' => substr($dd, strlen($date)),
-            ];
+            $entry_url = CRM_Utils_System::url('civicrm/admin/logviewer/logentry', $query = 'lineNumber=' . $line);
+            $msg = trim(substr($dd, strlen($date)));
+
+            // There could be empty messages, e.g. just "[error]".
+            if (preg_match('/^\[(\w+)\]\s*(.*)/', $msg, $matches)) {
+              $severity = $matches[1];
+              $msg = $matches[2];
+            }
+            else {
+              // There should only be the konwn severities - but we provide a fallback nonetheless.
+              $severity = 'UNKNOWN';
+            }
+            if (array_key_exists($severity, $severities_found)) {
+              $severities_found[$severity]++;
+            }
+            else {
+              $severities_found[$severity] = 1;
+            }
+
+            if (empty($filter_severity)
+              || in_array($severity, $filter_severity)
+            ) {
+              $entries[$line] = [
+                'lineNumber' => '<a href="' . $entry_url . '">' . $line . '</a>',
+                'dateTime' => $date,
+                'message' => $msg,
+                'severity' => $severity,
+              ];
+            }
           }
         }
       }
       fclose($handle);
       krsort($entries);
-      $this->assign('logEntries', $entries);
     }
+    $this->assign('logEntries', $entries);
+
+    // Add JS to use Datatable.
+    CRM_Core_Resources::singleton()->addScriptFile(E::LONG_NAME, 'js/logviewer.js'); #, 1, 'html-header');
+
     parent::run();
+  }
+
+  /**
+   * Get log level array.
+   *
+   * @return array
+   *   Log levels with Psr::Log::LogLevel as key and corresponding label as value,
+   *   starting with emergency.
+   */
+  protected function getLogLevels() {
+    $severities = CRM_Utils_Check::getSeverityOptions();
+    $log_levels = [];
+
+    // Reverse order to start with emergency.
+    foreach (array_reverse($severities) as $severity) {
+      $log_levels[$severity['name']] = $severity['label'];
+    }
+
+    return $log_levels;
   }
 
 }
--- templates/CRM/Logviewer/Page/LogViewer.tpl
+++ templates/CRM/Logviewer/Page/LogViewer.tpl
@@ -1,25 +1,57 @@
-{crmButton p='civicrm/admin/logviewer' q='reset=1' icon='refresh'}{ts}Refresh{/ts}{/crmButton}
-{crmButton p='civicrm/admin/logviewer/download' q='reset=1' icon='download'}{ts}Download Log File{/ts}{/crmButton}
-
 <p>{ts 1=$currentTime}The current time is %1.{/ts}</p>
+<p>{ts 1=$fileName}Logfile: %1{/ts}</p>
+
+<form action="/civicrm/admin/logviewer" method="post" name="post" id="post" class="CRM_Logviewer_Page_LogViewer">
+  <div class="crm-section">
+    <div class="label"><label for="severity">Severity</label></div>
+    <div class="content">
+      <select multiple="multiple" name="severity[]" id="severity" class="crm-form-multiselect" size="{$options|@count}">
+        {foreach from=$options item=row}
+          <option value="{$row.key}" {if $row.selected}selected="selected"{/if}>{$row.label}</option>
+        {/foreach}
+      </select>
+    </div>
+    <div class="clear"></div>
+  </div>
+
+  <div class="crm-submit-buttons">
+    <button class="crm-form-submit default validate crm-button crm-button-type-submit btn-primary" value="1" type="submit" name="logviewer_submit" id="logviewer_submit">
+      <i aria-hidden="true" class="crm-i fa-search"></i>{ts}Filter{/ts}</button>
+    <button class="crm-form-submit validate crm-button crm-button-type-submit btn-secondary" value="1" type="submit" name="logviewer_reset" id="logviewer_reset">
+      <i aria-hidden="true" class="crm-i fa-reset"></i>{ts}Reset{/ts}</button>
+    {crmButton p='civicrm/admin/logviewer/download' q='reset=1' icon='download'}{ts}Download Log File{/ts}{/crmButton}
+  </div>
+</form>
 
 {if $logEntries}
-    <table>
-    <tr class="columnheader">
+  <p>{ts 1=$logEntries|@count}Found %1 result(s).{/ts}</p>
+  <table id="logviewer">
+    <thead>
+      <tr class="columnheader">
         <th>{ts}Row{/ts}</th>
+        <th>{ts}Severity{/ts}</th>
         <th>{ts}Date{/ts}</th>
         <th>{ts}Message{/ts}</th>
+        {* Add extra column, because DataTable right-aligns the last column *}
         <th></th>
-    </tr>
-
-    {foreach from=$logEntries item=row}
-    <tr class="{cycle values="odd-row,even-row"}">
-        <td>{$row.lineNumber}</td>
-        <td>{$row.dateTime}</td>
-        <td>{$row.message}</td>
-    </tr>
-    {/foreach}
-    </table>
+      </tr>
+    </thead>
+    <tbody>
+      {foreach from=$logEntries item=row}
+        <tr class="{cycle values="odd-row,even-row"}">
+          <td>{$row.lineNumber}</td>
+          <td>{$row.severity}</td>
+          <td>{$row.dateTime}</td>
+          <td>{$row.message}</td>
+          <td></td>
+        </tr>
+      {/foreach}
+    </tbody>
+  </table>
+{elseif $noSearch}
+  <p>{ts}<em>Filter</em> for results.{/ts}</p>
+{elseif $logFound}
+  <p>{ts}Found no entries.{/ts}</p>
 {else}
   <p>{ts 1=$fileName}Unable to read entries from logfile at %1{/ts}</p>
 {/if}
